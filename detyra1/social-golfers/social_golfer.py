12fba2bb40a99f647a4abf89e8c4ade8bcd84206
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-11-04 23:12:30 +0100

    Added week_feasible forward checking for pruning

--- detyra1/social-golfers/social_golfer.py
+++ detyra1/social-golfers/social_golfer.py
@@ -1,37 +1,53 @@
 from itertools import combinations
 
 def pure_dfs_social_golfers(G=8, S=4, W=9):
-
     N = G * S
-    golfers = list(range(N))
-
-
     weeks = [[[] for _ in range(G)] for _ in range(W)]
-
-
     pairs_used = set()
-
 
     def pkey(a, b):
         if a > b: a, b = b, a
         return a * N + b
 
-    def place(w, gi, si, used_this_week):
+    def week_feasible(w, used_this_week):
+        for gi in range(G):
+            group = weeks[w][gi]
+            k = len(group)
+            if k == S:
+                continue
+            need = S - k
+            legal = 0
+            for x in range(N):
+                if x in used_this_week:
+                    continue
+                ok = True
+                for m in group:
+                    if pkey(x, m) in pairs_used:
+                        ok = False
+                        break
+                if ok:
+                    legal += 1
+                    if legal >= need:
+                        break
+            if legal < need:
+                return False
+        return True
 
+    def place(w, gi, si, used_this_week):
         if w == W:
             return True
         if gi == G:
             return place(w + 1, 0, 0, set())
-
         if si == S:
             return place(w, gi + 1, 0, used_this_week)
 
+        group_members = weeks[w][gi]
 
         for x in range(N):
             if x in used_this_week:
                 continue
 
-            group_members = weeks[w][gi]
+            # pair-once check vs current members
             bad = False
             for m in group_members:
                 if pkey(x, m) in pairs_used:
@@ -40,22 +56,20 @@ def pure_dfs_social_golfers(G=8, S=4, W=9):
             if bad:
                 continue
 
-
             weeks[w][gi].append(x)
             used_this_week.add(x)
 
             new_pairs = []
-            for m in group_members:
+            for m in group_members[:-1]:
                 pk = pkey(x, m)
                 pairs_used.add(pk)
                 new_pairs.append(pk)
-
 
             if place(w, gi, si + 1, used_this_week):
                 return True
 
             for pk in new_pairs:
-                pairs_used.remove(pk)
+                pairs_used.discard(pk)
             used_this_week.remove(x)
             weeks[w][gi].pop()
 
