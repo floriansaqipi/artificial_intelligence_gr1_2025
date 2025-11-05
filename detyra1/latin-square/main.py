35bd58aaca23b027617c33b163c3488a3ddc9d9c
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-11-05 08:44:29 +0100

    update latin-square main, to include timer

--- detyra1/latin-square/main.py
+++ detyra1/latin-square/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+import time
 from sys import argv
 from utils import print_latin_square
 from latin_square import iddfs_solve_latin_square
     
 if __name__ == "__main__":
+    start = time.time()
     n = int(argv[1]) if len(argv) > 1 else 3
     solutions = iddfs_solve_latin_square(n)
-    # for i, sol in enumerate(solutions):
-    #     print(f"Solution {i + 1}:")
-    #     print_latin_square(sol)
-    #     print()
+    end = time.time()
+
+    print(f'Koha totale e ekzekutimit: {end - start:.4f} sekonda.')
