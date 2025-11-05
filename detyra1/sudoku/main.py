632ba0aff1903456f5dc12c6cced288fc9ed7131
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-11-05 08:43:07 +0100

    easy, medium, hard mode u shtu, tash boll procrastination babush vakti me bo bfs🥲 part 2

---
+++ detyra1/sudoku/main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from sys import argv
+from sudoku_board import SudokuBoard
+
+def main(grid):
+   print("duke zgjidhur sudoku me bfs...") 
+
+if __name__ == "__main__":
+    easy_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+
+    medium_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+
+    hard_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+    
+    difficulty = argv[1] if len(argv) > 1 else "easy"
+
+    if difficulty == 'hard':
+        grid_to_solve = hard_grid
+        print("Jemi tu solve per hard grid")
+    elif difficulty == 'medium':
+        grid_to_solve = medium_grid
+        print("Jemi tu solve per medium grid")
+    else: 
+        grid_to_solve = easy_grid
+        print("Jemi tu solve per easy grid")
+
+    board = SudokuBoard(grid_to_solve)
+    print(f"Boardi fillestar eshte:\n{board}\n")
+
+    board.set_cell(0, 2, 4)
+    print(f"Boardi pas vendosjes se 4 ne koordinatat (1, 3):\n{board}\n")
