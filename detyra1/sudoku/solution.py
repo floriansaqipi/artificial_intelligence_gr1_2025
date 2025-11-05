e57f4e5b31460d25518f50ebf47b822d93a08bb8
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-11-05 11:13:51 +0100

    U KRY SUDOKU BABOOOOOOOOOOOOO (po mdoket, se kom testu me diqka qe so easy)

--- detyra1/sudoku/solution.py
+++ detyra1/sudoku/solution.py
@@ -1,17 +1,29 @@
 from sudoku_board import SudokuBoard
 from collections import deque
+from copy import deepcopy
+import traceback
 
 def bfs_with_pruning(board: SudokuBoard) -> SudokuBoard:
-    root_state = board
+    root = deepcopy(board)
     queue = deque()
-    queue.append(root_state)
+
+    queue.append(root)
+    counter = 1
     while queue:
         current_state = queue.popleft()
+        coords = current_state.next_coords()
 
-        if not is_valid(current_state):
-            continue # prune dat shiiiihhhh
-       
+        if coords is None:
+            break
+
+        next_x, next_y = coords
         
+        for val in range (1,10):
+            next_state = deepcopy(current_state)
+            try:
+                next_state.set_cell(next_x, next_y, val)
+                queue.append(next_state)
+            except Exception as e:
+                continue
 
-        for move in current_state.next_moves():
-            queue.append(move)
+    return current_state
